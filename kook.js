ab191f6a0f4da78726a33d1068b57c22dba588f4
ghada9990 <gdooo2266@hotmail.com>
2022-02-11 14:24:47 +0300

    gp

--- kook.js
+++ kook.js
@@ -13,12 +13,41 @@ function shuffle(array) {
 }
 
 // varible
+var flg = true;
+var arr= [];
 
+for(let i=0; i<allDivs.length; i++){
+    allDivs[i].addEventListener('click', function() {
+        if(flg){
+            this.firstChild.style.opacity = '1';
+            if(arr.length == 0){
+                arr[0] =this;
+            }
+            else if(arr.length == 1){
+                arr[1] = this;
+            }
+            if(arr.length == 2){
+                flg =false;
+            }
+        }
+        else{
+            return;
+        }
+        
+    })
+}
 
 //functions
 
-
-
-
+function chak(){
+    if (arr[0].firstChild.getAttribute('card') == arr[1].firstChild.getAttribute('card'))
+    alert('ok')
+else{
+    arr[0].firstChild.style.opacity =0;
+    arr[1].firstChild.style.opacity =1;
+    
+}
+arr =[];
+flg= true;}
 
 // event listeners
